9e2f8fb4190f24566677c6a24a4a6509042edc26
Filip Kniwel <95308357+MrPhilipp0@users.noreply.github.com>
2022-04-26 20:16:45 +0200

    Add files via upload

---
+++ UI.js
@@ -0,0 +1,126 @@
+class UI extends Calculator {
+  constructor() {
+    super();
+  }
+}
+
+const calc = new UI();
+
+// wpisywanie liczb z buttonów
+numberBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if (String(calc.inp).length < 16) {
+      if (String(calc.inp).indexOf('.') !== -1) {
+        calc.inp = calc.inp + btn.innerText;
+      } else {
+        calc.inp = Number(calc.inp + btn.innerText);
+      }
+      calc.render();
+    }
+  })
+});
+
+// wpisywanie liczb z klawiatury
+document.addEventListener('keypress', e => {
+  if (String(calc.inp).length < 16) {
+    const numButton = e.key;
+    if (!isNaN(Number(numButton))) {
+      if (String(calc.inp).indexOf('.') !== -1) {
+        calc.inp = calc.inp + numButton;
+      } else {
+        calc.inp = Number(calc.inp + numButton);
+      }
+    } else {
+      switch (numButton) {
+        case '.':
+          if (String(calc.inp).indexOf('.') === -1) calc.inp = calc.inp + '.';
+          break;
+        case '+':
+          calc.addition();
+          break;
+        case '-':
+          calc.subtraction();
+          break;
+        case '*':
+          calc.multiplication();
+          break;
+        case '/':
+          calc.division();
+          break;
+        case '^':
+          calc.power();
+          break;
+        case 'e':
+          calc.inp = calc.eNum;
+          break;
+        case 'Enter':
+        case '=':
+          calc.equalTo();
+          break;
+        case 'c':
+          calc.clean();
+          break;
+        default:
+          break;
+      }
+    }
+  
+    calc.render();
+  }
+})
+
+// obsługa kropki
+dot.addEventListener('click', () => {
+  if (String(calc.inp).indexOf('.') === -1) calc.inp = calc.inp + '.';
+  calc.render();
+});
+
+// obsługa operatorów
+handleBtns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    const eId = e.target.id;
+    switch (eId) {
+      case 'del':
+        calc.delete();
+        break;
+      case 'cleanAll':
+        calc.clean();
+        break;
+      case 'plus':
+        calc.addition();
+        break;
+      case 'minus':
+        calc.subtraction();
+        break;
+      case 'multi':
+        calc.multiplication();
+        break;
+      case 'divide':
+        calc.division();
+        break;
+      case 'pow':
+        calc.power();
+        break;
+      case 'sqrt':
+        calc.squareRoot();
+        break;
+      case 'eNum':
+        calc.inp = calc.eNum;
+        calc.render();
+        break;
+      case 'piNum':
+        calc.inp = calc.piNum;
+        calc.render();
+        break;
+      case 'equal':
+        calc.equalTo();
+        break;
+      case 'plusMinus':
+        calc.plusMinus();
+      default:
+        break;
+    }
+  });
+});
+
+
---
+++ calculator.js
@@ -0,0 +1,111 @@
+const numberBtns = document.querySelectorAll('.number');
+const input = document.getElementById('input');
+const answer = document.getElementById('answer');
+const symbol = document.getElementById('symbol');
+const handleBtns = document.querySelectorAll('.handleBtn');
+const dot = document.querySelector('.dot');
+
+class Calculator {
+  constructor() {
+    this.sym = null; // symbol
+    this.inp = 0; // input
+    this.ans = null; // answer
+    this.eNum = 2.7182;
+    this.piNum = 3.1415;
+  }
+
+  render() {
+    input.innerText = this.inp;
+    answer.innerText = this.ans;
+    symbol.innerText = this.sym;
+  } 
+
+  delete() {
+    const number = Math.floor(this.inp/10);
+    this.inp = number;
+    this.render();
+  }
+
+  clean() {
+    this.inp = 0;
+    this.ans = null;
+    this.sym = null;
+    this.render();
+  }
+
+  operation(char, mainOperation) {
+    this.inp = Number(this.inp);
+    this.ans = this.ans ? mainOperation : this.inp;
+    this.inp = 0;
+    this.sym = char;
+  }
+
+  addition() {
+    const add = this.ans + this.inp;
+    this.operation('+', add);
+    this.render();
+  }
+
+  subtraction() {
+    const minus = this.ans - this.inp;
+    this.operation('-', minus);
+    this.render();
+  }
+
+  multiplication() {
+    const multi = this.ans * this.inp;
+    this.operation('*', multi);
+    this.render();
+  }
+
+  division() {
+    const div = this.ans / this.inp;
+    this.operation('/', div);
+    this.render();
+  }
+
+  power() {
+    const pow = this.ans ** this.inp;
+    this.operation('^', pow);
+    this.render();
+  }
+
+  squareRoot() {
+    const root = Math.sqrt(this.inp);
+    this.ans = root;
+    this.inp = 0;
+    this.sym = 'sqrt';
+    this.render();
+  }
+
+  equalTo() {
+    this.inp = Number(this.inp);
+    switch (this.sym) {
+      case '+':
+        this.ans += this.inp;
+        break;
+      case '-':
+        this.ans -= this.inp;
+        break;
+      case '*':
+        this.ans *= this.inp;
+        break;
+      case '/':
+        this.ans /= this.inp;
+        break;
+      case '^':
+        this.ans = this.ans ** this.inp;
+        break;
+      default:
+        break;
+    }
+    this.inp = 0;
+    this.sym = null;
+    this.render();
+  }
+
+  plusMinus() {
+    this.inp = this.inp * -1;
+    this.render();
+  }
+}
